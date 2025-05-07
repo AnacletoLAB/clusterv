173e27ac47c56d74d3738ea18c7e6706f0e73887
jessica <jessica.gliozzo@gmail.com>
2025-05-07 22:19:19 +0200

    Removed install.R
    It is not necessary in modern packages.

--- install.R
+++
@@ -1,7 +0,0 @@
-if (require("MASS")==FALSE) 
-	stop("The library clsuterv requires library MASS");
-if (require("cluster")==FALSE) 
-	stop("The library clusterv requires library cluster");
-cat("Library clusterv loaded. \n")
-	 
-
